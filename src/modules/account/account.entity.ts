2ceb1c06d548d2524564d0c46071d4edb2f199a3
shinka <thomas.shinkarenko@gmail.com>
2022-02-20 21:50:56 -0300

    feat: add account entity

---
+++ src/modules/account/account.entity.ts
@@ -0,0 +1,28 @@
+import { Exclude } from 'class-transformer';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('account')
+export class Account {
+  @Exclude()
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Exclude()
+  @Column({ default: null, select: false })
+  password: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  cpf: string;
+
+  @Column({ unique: true, nullable: true })
+  email: string;
+
+  @Column({ unique: true, nullable: true })
+  phone: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+}
