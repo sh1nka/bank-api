f306c756fc0c1302e3df1f725d7df386295a0de0
shinka <thomas.shinkarenko@gmail.com>
2022-02-20 21:50:06 -0300

    feat: add common messages

---
+++ src/common/messages.ts
@@ -0,0 +1,3 @@
+export const CPF_LENGTH = 'CPF deve possuir exatamente 11 caracteres';
+export const CPF_ACCOUNT_ALREADY_CREATED = 'CPF já vinculado à uma conta';
+export const INCORRECT_CREDENTIALS = 'CPF ou senha incorretos';
