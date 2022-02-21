ca64926e270059f7ecf9f841cdaec55c9dce2ed0
shinka <thomas.shinkarenko@gmail.com>
2022-02-20 22:56:02 -0300

    chore: dir name

--- src/modules/account/account.module.ts
+++ src/modules/account/account.module.ts
@@ -1,7 +1,7 @@
 import { AuthSignUpService } from '@modules/auth/services/sign-up-auth.service';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AccountController } from './controller/account.controller';
+import { AccountController } from './controllers/account.controller';
 import { AccountRepository } from './repositories/account.repository';
 import { CreateAccountService } from './services/create-account.service';
 
