d5f014012056cdc1121e9b2fe04c94a00b7e85f5
shinka <thomas.shinkarenko@gmail.com>
2022-02-20 21:49:57 -0300

    feat: add schema validation, conn provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
+import { BankApiDbProvider } from '@config/connection/bank-api-db.connection';
+import { configValidationSchema } from '@config/connection/config.schema';
+import { AccountModule } from '@modules/account/account.module';
+import { AuthModule } from '@modules/auth/auth.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
+    }),
+    ...BankApiDbProvider,
+    AuthModule,
+    AccountModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
