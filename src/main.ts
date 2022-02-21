cd0a85bdfdef58fa196e42238b94848d6827dfed
shinka <thomas.shinkarenko@gmail.com>
2022-02-20 21:48:58 -0300

    feat: add main settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('bank/api/v1');
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  await app.listen(3000, () =>
+    Logger.verbose(`Bank API is running on port 3000`),
+  );
 }
 bootstrap();
