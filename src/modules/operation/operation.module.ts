8aeabd4b5d7694f5c9b00151b2037d22767e79cd
shinka <thomas.shinkarenko@gmail.com>
2022-02-21 21:36:30 -0300

    feat: add operation services

--- src/modules/operation/operation.module.ts
+++ src/modules/operation/operation.module.ts
@@ -1,10 +1,14 @@
+import { AccountRepository } from '@modules/account/repositories/account.repository';
 import { AuthModule } from '@modules/auth/auth.module';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { OperationController } from './controllers/operation.controller';
+import { OperationDepositService } from './services/deposit-operation.service';
+import { OperationTransferService } from './services/transfer-operation.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [TypeOrmModule.forFeature([AccountRepository]), AuthModule],
   controllers: [OperationController],
-  providers: [],
+  providers: [OperationDepositService, OperationTransferService],
 })
 export class OperationModule {}
