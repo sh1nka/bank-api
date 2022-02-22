c280f77686740888c5d166010a5fad49bf13497e
shinka <thomas.shinkarenko@gmail.com>
2022-02-21 21:35:08 -0300

    feat: add new messages

--- src/common/messages.ts
+++ src/common/messages.ts
@@ -1,3 +1,9 @@
 export const CPF_LENGTH = 'CPF deve possuir exatamente 11 caracteres';
 export const CPF_ACCOUNT_ALREADY_CREATED = 'CPF já vinculado à uma conta';
 export const INCORRECT_CREDENTIALS = 'CPF ou senha incorretos';
+export const ACCOUNT_NOT_FOUND = 'Conta não encontrada';
+export const INSUFFICIENT_BALANCE = 'Saldo insuficiente';
+export const VALUE_MAX_DEPOSIT_LIMIT =
+  'O valor excede o limite de depósito (R$ 2000,00)';
+export const VALUE_MAX_TRANSFER_LIMIT =
+  'O valor excede o limite de transferência (R$ 2000,00)';
