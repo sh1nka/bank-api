b4cfc8952bf9cb2b39fea7b7ee519ceb5df2ac26
shinka <thomas.shinkarenko@gmail.com>
2022-02-21 21:37:59 -0300

    misc: remove unused

--- src/modules/operation/services/deposit-operation.service.ts
+++ src/modules/operation/services/deposit-operation.service.ts
@@ -1,12 +1,38 @@
+import { ACCOUNT_NOT_FOUND, VALUE_MAX_DEPOSIT_LIMIT } from '@common/messages';
+import { Account } from '@modules/account/account.entity';
 import { AccountRepository } from '@modules/account/repositories/account.repository';
-import { Injectable } from '@nestjs/common';
-import { DepositOperationDto } from '../dto/deposit-operation.dto';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { DepositDto } from '../dto/deposit.dto';
 
 @Injectable()
 export class OperationDepositService {
-  constructor(private readonly accountReposiroty: AccountRepository) {}
+  constructor(private readonly accountRepository: AccountRepository) {}
 
-  async deposit(body: DepositOperationDto): Promise<any> {
-    return;
+  async deposit(account: Account, body: DepositDto): Promise<any> {
+    try {
+      const accountFound = await this.accountRepository.findAccountCpf(
+        account.cpf,
+      );
+      if (!accountFound) {
+        throw new NotFoundException(ACCOUNT_NOT_FOUND);
+      }
+      const { value } = body;
+      const limitValue = value > 2000 ? true : false;
+      if (limitValue) {
+        throw new Error(VALUE_MAX_DEPOSIT_LIMIT);
+      }
+      const balanceUpdate = {
+        id: account.id,
+        balance: account.balance + value,
+      };
+      await this.accountRepository.updateBalance(balanceUpdate);
+      return;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
   }
 }
