a37d4b11c6ced4bcdc0e4c49edb5d3133274452d
shinka <thomas.shinkarenko@gmail.com>
2022-02-21 21:35:31 -0300

    misc: remove console logs

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -31,7 +31,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       if (!account) {
         throw new UnauthorizedException(INCORRECT_CREDENTIALS);
       }
-      console.log('validou');
       return account;
     } catch (error) {
       throw new InternalServerErrorException();
