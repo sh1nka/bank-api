82bb87aeb3c4fb6665e55ef80596aa44139edc72
shinka <thomas.shinkarenko@gmail.com>
2022-02-22 22:11:16 -0300

    misc: unused file

--- src/modules/account/services/check-account-exists.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AccountRepository } from '../repositories/account.repository';
-
-@Injectable()
-export class checkAccountExistsService {
-  constructor(private readonly accountRepository: AccountRepository) {}
-}
